fe4f7435cd1d815707a056b954dfb5795185d540
Your Name <you@example.com>
2023-04-30 11:29:39 +0300

    💬 Обработка ошибки

--- admin.py
+++ admin.py
@@ -3,7 +3,15 @@
 from vkbottle.bot import Bot
 
 async def check(bot: Bot, peer_id: int, user_id: int) -> bool:
-    response = await bot.api.messages.get_conversation_members(peer_id=peer_id)
+    try:
+        response = await bot.api.messages.get_conversation_members(peer_id=peer_id)
+    except Exception as e:
+        await bot.api.messages.send(
+            peer_id=peer_id, 
+            message="Ошибка при получении списка участников беседы. Возможно, бот не администратор."
+            )
+        raise e
+        
     for item in response.items:
         if item.member_id == user_id:
             return item.is_admin

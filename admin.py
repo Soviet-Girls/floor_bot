21e0fe4014ae82007815083bca159399bdea0108
Your Name <you@example.com>
2023-04-30 10:10:22 +0300

    🤫 Быстрофикс проверки на админа

--- admin.py
+++ admin.py
@@ -6,6 +6,4 @@ async def check(bot: Bot, peer_id: int, user_id: int) -> bool:
     response = await bot.api.messages.get_conversation_members(peer_id=peer_id)
     for item in response.items:
         if item.member_id == user_id:
-            if item.is_admin: return True
-        else:
-            return False
+            return item.is_admin

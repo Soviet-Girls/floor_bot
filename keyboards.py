b9796908f227e35555ce42bafb44ca6c0a16b6d6
Your Name <you@example.com>
2023-05-01 10:22:37 +0300

    🖱️ Две новые кнопки: обновления данных и графика

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,21 @@
 # Пресеты клавиатур для сообщений
 
-from vkbottle import Keyboard, OpenLink
+from vkbottle import Keyboard, OpenLink, Callback
 
 market_links = (
     Keyboard(inline=True)
     .add(OpenLink(link="https://rarible.com/Cryptospotty/", label="Rarible"))
     .add(OpenLink(link="https://opensea.io/collection/cryptospotty-1", label="OpenSea"))
+    .row()
+    .add(Callback("🔃 Обновить", payload={"command": "update_full"}))
+)
+
+
+market_links_conversation = (
+    Keyboard(inline=True)
+    .add(OpenLink(link="https://rarible.com/Cryptospotty/", label="Rarible"))
+    .add(OpenLink(link="https://opensea.io/collection/cryptospotty-1", label="OpenSea"))
+    .row()
+    .add(Callback("🔃 Обновить", payload={"command": "update"}))
+    .add(Callback("📈 График", payload={"command": "chart"}))
 )
--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 import os
 import asyncio
+import random
 
 from dotenv import load_dotenv
 from typing import Tuple
 
-from vkbottle.bot import Bot, Message
+
+from vkbottle import GroupEventType
+from vkbottle.bot import Bot, Message, MessageEvent
 from vkbottle.dispatch.rules.base import CommandRule
 from vkbottle.tools import PhotoMessageUploader
 
@@ -42,7 +45,7 @@ async def now_handler(message: Message):
         if is_admin == False:
             await message.answer("У вас нет доступа к этой команде")
             return
-        await message.answer(bot_message, keyboard=keyboards.market_links)
+        await message.answer(bot_message, keyboard=keyboards.market_links_conversation)
 
 
 # Отправлять флор каждые n минут
@@ -74,7 +77,56 @@ async def stop_handler(message: Message):
     schedule_peer_id = 0
     await message.answer("Бот остановлен")
 
+# Обработка callback
+@bot.on.raw_event(GroupEventType.MESSAGE_EVENT, dataclass=MessageEvent)
+async def chart_handler(event: MessageEvent):
+    print(event.object.payload)
+    
+    # Обновить флор в сообщении
+    if event.object.payload.get("command") == "update":
+        bot_message = await floor.get()
+        await bot.api.messages.edit(
+            peer_id=event.object.peer_id,
+            conversation_message_id=event.conversation_message_id,
+            message=bot_message,
+            keyboard=keyboards.market_links_conversation,
+        )
+        await event.show_snackbar("Флор обновлен!")
+        return
+    
+    # Обновить флор в личных сообщениях
+    if event.object.payload.get("command") == "update_full":
+        bot_message = await floor.get()
+        raw_data = await floor.get_raw()
+        buf = chart.generate(raw_data)
+        attachment = await uploader.upload(buf, peer_id=event.object.peer_id)
+        await bot.api.messages.edit(
+            peer_id=event.object.peer_id,
+            conversation_message_id=event.conversation_message_id,
+            message=bot_message,
+            attachment=attachment,
+            keyboard=keyboards.market_links,
+        )
+        await event.show_snackbar("Флор обновлен!")
+        return
 
+    raw_data = await floor.get_raw()
+    buf = chart.generate(raw_data)
+    try:
+        attachment = await uploader.upload(buf, peer_id=event.object.user_id)
+        await bot.api.messages.send(
+            peer_id=event.object.user_id,
+            attachment=attachment,
+            keyboard=keyboards.market_links,
+            random_id=random.randint(0, 2 ** 64),
+        )
+        await event.show_snackbar("График отправлен в личные сообщения!")
+        return
+    except Exception as e:
+        await event.show_snackbar("Не удалось отправить график! Возможно, у вас нет диалога с ботом.")
+        raise e
+
+# Проверка на права администратора
 @bot.on.message(text="/admin")
 async def admin_handler(message: Message):
     is_admin = await admin.check(bot, message.peer_id, message.from_id)

066b8a8772e3e6b281f542b5943ee11f0f78e1b9
buvanenko <gleb@buvanenko.ru>
2024-02-25 09:45:17 +0300

    Rubles in widget

--- vk/widget.py
+++ vk/widget.py
@@ -1,5 +1,6 @@
 from vkbottle.bot import Bot
 from data.floor import get_stats, get_boys_stats
+import data.currency as currency
 
 from config import config
 
@@ -34,6 +35,9 @@ community_bot = Bot(token=config.widget.token)
 async def generate_code():
     stats_girls = await get_stats()
     stats_boys = await get_boys_stats()
+    matic_rub, matic_usd = await currency.get_matic_rate()
+    stats_girls['volume'] = "%.2f" % stats_girls['volume']*matic_rub/1000
+    stats_boys['volume'] = "%.2f" % stats_boys['volume']*matic_rub/1000
     widget = {
         "title": "Статистика коллекции",
         "title_url": config.widget.link,
@@ -49,15 +53,15 @@ async def generate_code():
         "body": [
             [
                 {"text": "♀️ Soviet Girls", "url": "https://vk.com/@sovietgirls_nft-about"},
-                {"text": f"{'%.2f' % float(stats_girls['volume'])}"},
-                {"text": f"{stats_girls['floorPrice']} MATIC"},
+                {"text": f"{stats_girls['volume']}К ₽"},
+                {"text": f"{int(stats_girls['floorPrice']*matic_rub)} ₽"},
                 {"text": f"{stats_girls['items']}"},
                 {"text": f"{stats_girls['owners']}"}
             ],
             [
                 {"text": "♂️ Soviet Boys", "url": "https://vk.com/@sovietgirls_nft-soviet-boys"},
-                {"text": f"{'%.2f' % float(stats_boys['volume'])}"},
-                {"text": f"{stats_boys['floorPrice']} MATIC"},
+                {"text": f"{stats_boys['volume']}К ₽"},
+                {"text": f"{int(stats_boys['floorPrice']*matic_rub)} ₽"},
                 {"text": f"{stats_boys['items']}"},
                 {"text": f"{stats_boys['owners']}"}
             ],

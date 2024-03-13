2ca86e30bf88b8a5609544b1c1355bfababda7b6
buvanenko <gbuvanenko@gmail.com>
2024-03-13 19:50:40 +0300

    Update stories pic

--- data/stories.py
+++ data/stories.py
@@ -7,11 +7,12 @@ from PIL import Image, ImageDraw, ImageFont
 def generate_image(floor, volume, owners, tokens) -> io.BytesIO:
     img = Image.open("stories.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("XO_Tahion_Nu.ttf", 85)
-    draw.text((458+10, 1153-5), str(floor), (0, 0, 0), font=font)
-    draw.text((539+10, 1261-5), str(volume), (0, 0, 0), font=font)
-    draw.text((728+10, 1368-5), str(owners), (0, 0, 0), font=font)
-    draw.text((563+10, 1475-5), str(tokens), (0, 0, 0), font=font)
+    font = ImageFont.truetype("Bahnschrift-BoldCondensed.ttf", 85)
+    draw.text((630, 353), str(floor), (0, 0, 0), font=font)
+    draw.text((630, 453), str(volume), (0, 0, 0), font=font)
+    draw.text((630, 555), str(owners), (0, 0, 0), font=font)
+    draw.text((630, 655), str(tokens), (0, 0, 0), font=font)
+
     buf = io.BytesIO()
     img.save(buf, format="PNG")
     buf.seek(0)
